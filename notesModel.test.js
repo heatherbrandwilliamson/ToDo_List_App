7b175e35b227cc33b3111948707a2847260f4275
heatherbrandwilliamson <heather.brandwilliamson@gmail.com>
2023-06-19 15:46:09 +0100

    tests added

--- notesModel.test.js
+++ notesModel.test.js
@@ -13,5 +13,19 @@ describe('notesModel tests', () => {
         expect(model.list).toEqual(notes);
       });
 
+    it('should return the empty list of notes', () => {
+        model.addNote('Buy milk');
+        model.addNote('Go to the gym');
+        model.getNotes();
+        expect(model.list).toEqual(['Buy milk', 'Go to the gym']);
+      });
+
+      it('should return the empty list of notes', () => {
+        model.reset();
+        model.getNotes();
+        const notes = []
+        expect(model.list).toEqual(notes);
+      });  
+
 });
 
